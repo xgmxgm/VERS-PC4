2818c9ea56c4a36b8981e1c1cf1fee4c71fa093f
XGM <109265751+xgmxgm@users.noreply.github.com>
2024-08-28 19:39:29 +0600

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'Device',
+	title: 'VERS-PC4',
 	description: 'Сontrol and monitoring device',
 }
 
